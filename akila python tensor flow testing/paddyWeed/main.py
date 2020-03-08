5012b8fb2b2c8f1741e5f2713182caa542d7ac36
akilanana1235 <akila.2018400@iit.ac.lk>
2020-03-08 09:17:55 +0530

    Update main.py

--- akila python tensor flow testing/paddyWeed/main.py
+++ akila python tensor flow testing/paddyWeed/main.py
@@ -6,4 +6,4 @@ import numpy as np
 fashion_mist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, trst_labels) = fashion_mist.load_data()
 
-train_images.shape
+train_images.shape 
