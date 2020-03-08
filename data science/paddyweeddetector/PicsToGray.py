aff2edf473196cf70ba66c6e1a81c20bbb9f76bc
amilakelum <50689898+amilakelum@users.noreply.github.com>
2020-03-08 09:08:57 +0530

    Adding the Data Science part

---
+++ data science/paddyweeddetector/PicsToGray.py
@@ -0,0 +1,61 @@
+import numpy 
+import os
+import cv2
+import random
+import pickle
+
+file_list = []
+class_list = []
+
+DATADIR = "D:\paddyWeedDetector\Data set\set"
+CATEGORIES = ["weed", "paddy"]
+IMG_SIZE = 50
+
+for category in CATEGORIES :
+	path = os.path.join(DATADIR, category)
+	for img in os.listdir(path):
+		img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE) #reading the image and converting the image into gray scale
+
+training_data = []
+
+def create_training_data():
+	for category in CATEGORIES :
+		path = os.path.join(DATADIR, category)
+		class_num = CATEGORIES.index(category)
+		for img in os.listdir(path):
+			try :
+				img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+				new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+				training_data.append([new_array, class_num])
+			except Exception as e:
+				pass
+
+create_training_data()
+
+random.shuffle(training_data) #shuffle to reduce the time
+
+X = [] #features
+y = [] #labels
+
+for features, label in training_data:
+	X.append(features)
+	y.append(label)
+
+#resize the image for easy access
+X = numpy.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+# Creating the files containing all the information about the model
+#open pickle file only for write
+pickle_out = open("X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+#open pickle file only to read
+pickle_in = open("X.pickle", "rb")
+
+#load it to the variable
+X = pickle.load(pickle_in)
