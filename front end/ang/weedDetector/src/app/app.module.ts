ec184fbabc7607ff7b925ae99e2eebf0a8d94e4a
MalshamaPerera <55736409+MalshamaPerera@users.noreply.github.com>
2020-03-17 18:59:34 +0530

    front end
    front end

--- front end/ang/weedDetector/src/app/app.module.ts
+++ front end/ang/weedDetector/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstPageComponent } from './first-page/first-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { FirstPageComponent } from './first-page/first-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
