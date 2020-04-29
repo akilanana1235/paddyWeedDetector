b9547ef42caceef69b804f1f9c8c8ab803621b01
MalshamaPerera <55736409+MalshamaPerera@users.noreply.github.com>
2020-04-29 12:09:23 +0530

    Angular Routing
    Modifying the sign up component

--- front end/ang/weedDetector/src/app/signup/signup.component.ts
+++ front end/ang/weedDetector/src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signUpForm:FormGroup;
+  invalidLogin:boolean=false;
+  constructor(private formBuilder:FormBuilder,private router:Router) { }
 
   ngOnInit() {
+    this.signUpForm=this.formBuilder.group({
+      inputUserName4: ['',Validators.compose([Validators.required])],
+      inputPassword4:['', Validators.required] ,
+      inputAddress:['', Validators.required] ,
+      inputAddress2:['', Validators.required] ,
+      inputCity:['', Validators.required] ,
+      inputZip:['', Validators.required] 
+    });
+
+}
+onSubmit(){
+  console.log(this.signUpForm.value);
+  if(this.signUpForm.invalid){
+    return;
   }
 
+  const signInData = {
+    userNameData:this.signUpForm.controls.inputUserName4.value,
+    passwordData:this.signUpForm.controls.inputPassword4.value,
+    addressData:this.signUpForm.controls.inputAddress.value,
+    addressData2:this.signUpForm.controls.inputAddress2.value,
+    cityData:this.signUpForm.controls.inputCity.value,
+    zipData:this.signUpForm.controls.inputZip.value
+  }
+
+}
 }
