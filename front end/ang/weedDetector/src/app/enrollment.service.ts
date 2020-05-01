80134bf6227fc5c2e134390b20d1326a7487292f
MalshamaPerera <55736409+MalshamaPerera@users.noreply.github.com>
2020-05-01 23:55:52 +0530

    adding the complete front end

--- front end/ang/weedDetector/src/app/enrollment.service.ts
+++ front end/ang/weedDetector/src/app/enrollment.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {User} from './user';
+import {catchError} from 'rxjs/operators';
+import {throwError} from 'rxjs';
 
 
 @Injectable({
@@ -12,6 +14,11 @@ export class EnrollmentService {
   constructor(private _http:HttpClient) { }
 
   enroll(user:User){
-   return this._http.post<any>(this._url,user);
+   return this._http.post<any>(this._url,user)
+    .pipe(catchError(this.errorHandler))
+  }
+
+  errorHandler(error : HttpErrorResponse){
+    return throwError(error);
   }
 }
