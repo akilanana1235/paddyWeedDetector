97dc7ee100616fffd31b38ad58a0b82be838a490
Danushkaiit <48214099+Danushkaiit@users.noreply.github.com>
2020-05-02 13:43:25 +0530

    Delete serializers.py

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework importserializers
-from weedApp.models import File
-
-class fileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = File
-        fields = "__all__"
